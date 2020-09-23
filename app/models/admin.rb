f0815a90f2a8a9c09503d0d90d0eb430200d6d91
Priyadharshini <priyadharshini2695@gmail.com>
2020-09-23 14:07:24 -0400

    Setup devise

---
+++ app/models/admin.rb
@@ -0,0 +1,8 @@
+# Admin class manages admin part of the application including login, logout and register
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates_presence_of :email, :password
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
