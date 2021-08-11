81e0552ad0c5451dac43229c3bf92d89b7492ce5
rishitejar <77693402+rishitejar@users.noreply.github.com>
2021-08-11 15:00:00 -0400

    Update products_spec.rb

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,9 +1,9 @@
 require('rails_helper')
 
 RSpec.describe('Products', type: :request) do
-  describe 'GET /products/status/0' do
+  describe 'GET /products/status/:status' do
     it 'works!' do
-      get status_products_path(status: 0)
+      get status_products_path(:status => 'available')
       expect(response).to(have_http_status(200))
     end
   end
