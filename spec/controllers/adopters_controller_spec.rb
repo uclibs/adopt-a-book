350ef5b570cd02f1e9b77f28925c7757c289d8c8
rishitejar <rangarra@mail.uc.edu>
2021-09-13 14:22:22 -0400

    rubocop changes

--- spec/controllers/adopters_controller_spec.rb
+++ spec/controllers/adopters_controller_spec.rb
@@ -2,65 +2,65 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe AdoptersController, type: :controller do
-	before do
-		admin = FactoryBot.create(:admin)
-		@adopter = FactoryBot.create(:adopter)
-		sign_in_admin(admin)
-	end
+  before do
+    admin = FactoryBot.create(:admin)
+    @adopter = FactoryBot.create(:adopter)
+    sign_in_admin(admin)
+  end
 
-	def sign_in_admin(admin)
-		sign_in admin
-	end
+  def sign_in_admin(admin)
+    sign_in admin
+  end
 
-	let(:valid_attributes) do
-		{
-			fname: 'First Name',
-			lname: 'Last Name',
-			address: 'Address',
-			phone: '123-456-7890',
-			email: 'adopter@example.com',
-			product_id: '1'
-		}
-	end
+  let(:valid_attributes) do
+    {
+      fname: 'First Name',
+      lname: 'Last Name',
+      address: 'Address',
+      phone: '123-456-7890',
+      email: 'adopter@example.com',
+      product_id: '1'
+    }
+  end
 
-	let(:invalid_attributes) do
-		# todo
-	end
+  let(:invalid_attributes) do
+    # todo
+  end
 
-	let(:valid_session) { {} }
+  let(:valid_session) { {} }
 
-	describe 'GET #edit' do
-		it 'returns a success response' do
-			get :edit, params: { id: @adopter.to_param }
-			expect(response).to(be_successful)
-		end
-	end
+  describe 'GET #edit' do
+    it 'returns a success response' do
+      get :edit, params: { id: @adopter.to_param }
+      expect(response).to(be_successful)
+    end
+  end
 
-	describe 'PUT #update' do
-		context 'with valid params' do
-			let(:new_attributes) do
-				{
-					fname: 'First Name',
-					lname: 'Last Name',
-					address: 'Address',
-					phone: '123-456-7890',
-					email: 'adopter@example.com'
-				}
-			end
+  describe 'PUT #update' do
+    context 'with valid params' do
+      let(:new_attributes) do
+        {
+          fname: 'First Name',
+          lname: 'Last Name',
+          address: 'Address',
+          phone: '123-456-7890',
+          email: 'adopter@example.com'
+        }
+      end
 
-			it 'updates the requested Adopter' do
-				put :update, params: { id: @adopter.to_param, adopter: new_attributes }
-				expect(@adopter.fname).to(eq('First Name'))
-				expect(@adopter.lname).to(eq('Last Name'))
-				expect(@adopter.address).to(eq('Address'))
-				expect(@adopter.phone).to(have_content('123-456-7890'))
-				expect(@adopter.email).to(have_content('adopter@example.com'))
-			end
-		end
+      it 'updates the requested Adopter' do
+        put :update, params: { id: @adopter.to_param, adopter: new_attributes }
+        expect(@adopter.fname).to(eq('First Name'))
+        expect(@adopter.lname).to(eq('Last Name'))
+        expect(@adopter.address).to(eq('Address'))
+        expect(@adopter.phone).to(have_content('123-456-7890'))
+        expect(@adopter.email).to(have_content('adopter@example.com'))
+      end
+    end
 
-		it 'redirects to the product' do
-			put :update, params: { id: @adopter.to_param, adopter: valid_attributes }
-			expect(response).to redirect_to(product_path)
-		end
-	end
+    it 'redirects to the product' do
+      put :update, params: { id: @adopter.to_param, adopter: valid_attributes }
+      expect(response).to redirect_to(product_path)
+    end
+  end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,47 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-	include ActiveSupport::Testing::TimeHelpers
+  include ActiveSupport::Testing::TimeHelpers
 
-	describe 'initialize_session method' do
-		@controller = ApplicationController.new
-		it 'initializes cart session' do
-			@controller.instance_eval { initialize_session }
-			expect(session[:cart]).to be_empty
-		end
-	end
+  describe 'initialize_session method' do
+    @controller = ApplicationController.new
+    it 'initializes cart session' do
+      @controller.instance_eval { initialize_session }
+      expect(session[:cart]).to be_empty
+    end
+  end
 
-	describe 'check_session_expiry method' do
-		@controller = ApplicationController.new
-		it 'initializes expiry session' do
-			@controller.instance_eval { check_session_expiry }
-			expect(@controller.instance_eval { session[:expires_at] }).to be_within(1.second).of(ENV['CART_EXPIRY_TIME'].to_i.minutes.from_now)
-		end
+  describe 'check_session_expiry method' do
+    @controller = ApplicationController.new
+    it 'initializes expiry session' do
+      @controller.instance_eval { check_session_expiry }
+      expect(@controller.instance_eval { session[:expires_at] }).to be_within(1.second).of(ENV['CART_EXPIRY_TIME'].to_i.minutes.from_now)
+    end
 
-		before do
-			session[:cart] = { '1' => {} }
-			@product = FactoryBot.create(:pending_product)
-			@controller.instance_eval { initialize_session }
-			@controller.instance_eval { check_session_expiry }
-			travel_to((ENV['CART_EXPIRY_TIME'].to_i + 1.second).minutes.from_now)
-			@controller.instance_eval { check_session_expiry }
-		end
+    before do
+      session[:cart] = { '1' => {} }
+      @product = FactoryBot.create(:pending_product)
+      @controller.instance_eval { initialize_session }
+      @controller.instance_eval { check_session_expiry }
+      travel_to((ENV['CART_EXPIRY_TIME'].to_i + 1.second).minutes.from_now)
+      @controller.instance_eval { check_session_expiry }
+    end
 
-		it 'changes adopt status of the products in the cart to available' do
-			expect(Product.find(1).adopt_status).to eq('available')
-		end
+    it 'changes adopt status of the products in the cart to available' do
+      expect(Product.find(1).adopt_status).to eq('available')
+    end
 
-		it 'resets session after 30 minutes' do
-			expect(session).to be_empty
-		end
-	end
+    it 'resets session after 30 minutes' do
+      expect(session).to be_empty
+    end
+  end
 
-	describe 'logout' do
-		before do
-			@admin = FactoryBot.create(:admin)
-		end
-		it 'redirects to admin login page' do
-			expect(@controller.instance_eval { after_sign_out_path_for(@admin) }).to eq new_admin_session_path
-		end
-	end
+  describe 'logout' do
+    before do
+      @admin = FactoryBot.create(:admin)
+    end
+    it 'redirects to admin login page' do
+      expect(@controller.instance_eval { after_sign_out_path_for(@admin) }).to eq new_admin_session_path
+    end
+  end
 end
