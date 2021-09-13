a135dcc4c164e95e0899c5012ec187b7555712fd
rishitejar <rangarra@mail.uc.edu>
2021-09-13 14:30:59 -0400

    page_spec changes

--- spec/features/page_user_spec.rb
+++ spec/features/page_user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature 'pages form', type: :feature do
-  let(:page) { FactoryBot.create(:page) }
+  let(:test_page) { FactoryBot.create(:page) }
   it 'Visit the event/donor page' do
-    visit page_path(page)
+    visit page_path(test_page)
     expect(page).to have_text('MyString')
     expect(page).to have_text('MyText')
   end
